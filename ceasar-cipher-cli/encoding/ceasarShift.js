623415e6e8347e577b34c184c8d5fb605cc314c0
Viktar Svintsitsky <jetalai3@gmail.com>
2020-03-29 00:07:14 +0300

    feat: add task structure and ceasarShift module

---
+++ ceasar-cipher-cli/encoding/ceasarShift.js
@@ -0,0 +1,22 @@
+function ceasarShift(str, amount, action) {
+  let output = '';
+
+  amount = action === 'decode' ? -amount : amount;
+
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if (c.match(/[a-z]/i)) {
+      const code = str.charCodeAt(i);
+
+      if (code >= 65 && code <= 90) {
+        c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+      }
+    }
+    output += c;
+  }
+  return output;
+}
+
+module.exports = ceasarShift;
